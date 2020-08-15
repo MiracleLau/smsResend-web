35f1ed01813fb765f3d5a9969d0e42b70dc65fae
MiracleLau <zhiminemail@163.com>
2020-08-15 21:50:25 +0800

    添加测试功能

--- app/controller/Index.php
+++ app/controller/Index.php
@@ -16,14 +16,19 @@ class Index extends BaseController
             $data = request()->post();
             if(isset($data['pwd']) && $data["pwd"]== config('app.api_pwd')){
                 unset($data["pwd"]);
-                $md5 = md5(json_encode($data));
-                $count = Sms::where("md5",$md5)->count();
-                if($count == 0) {
-                    $sms = new Sms;
-                    $data["md5"] = $md5;
-                    $sms->save($data);
+                if(isset($data["test"])){
                     return get_result(0, "保存成功");
+                } else {
+                    $md5 = md5(json_encode($data));
+                    $count = Sms::where("md5",$md5)->count();
+                    if($count == 0) {
+                        $sms = new Sms;
+                        $data["md5"] = $md5;
+                        $sms->save($data);
+                        return get_result(0, "保存成功");
+                    }
                 }
+                
             } else {
                     return get_result(1, "密码不正确");
             }
